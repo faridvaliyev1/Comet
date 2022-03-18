ec8cccee5a471643cb8c8b7aae2d825fc266aca6
faridval <faridvaliyevgit@gmail.com>
2022-03-19 00:45:09 +0200

    Adding null_percentage formula

--- Utils/Formulas.py
+++ Utils/Formulas.py
@@ -1,13 +1,35 @@
-class Formulas(object):
+from ast import Pass
+
+
+class Formulas:
     
-    @staticmethod
     def metric_total_null(total_nulls,column,row):
         return total_nulls/(column*row)
     
-    @staticmethod
     def metric_total_null_column(total_nulls,column):
         return total_nulls/column
     
-    @staticmethod
     def metric_total_null_row(total_nulls,row):
         return total_nulls/row
+    
+    def null_percentage(Cluster,PropertyUsage):
+        Cluster_length=len(Cluster)
+        Cluster_max_count=PropertyUsage[Cluster[0]]
+        PropertyUsage_Sum=0
+        
+        for property in Cluster:
+            if Cluster_max_count<PropertyUsage[property]:
+                Cluster_max_count=PropertyUsage[property]
+            
+            PropertyUsage_Sum=PropertyUsage[property]
+        
+        cluster_null_percentage= ((Cluster_length-PropertyUsage_Sum)/(Cluster_length+1)*Cluster_max_count)
+        return cluster_null_percentage
+        
+        
+        
+        
+        
+            
+        
+        
